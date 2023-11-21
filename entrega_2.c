8967b7e36ce3cb35a5601266ab7d90d0ff8d5043
Ryes <33528286+dylanluigi@users.noreply.github.com>
2023-11-21 18:20:00 +0100

    Create entrega_2.c

---
+++ entrega_2.c
@@ -0,0 +1,331 @@
+/*
+Entrega 2
+Dylan Luigi Canning García
+Juan Marí González
+Antonio Marí González
+Descrición:
+1. Implementación Mini-Shell
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define COMMAND_LINE_SIZE 1024
+#define MAX_ARGS 64
+#define COMMAND_LINE_SIZE 1024
+#define MAX_ARGS 64
+
+int check_internal(char **args);
+int parse_args(char **args, char *line);
+int execute_line(char *line);
+char *read_line(char *line);
+void imprimir_prompt();
+int internal_cd(char **args);
+int internal_export(char **args);
+int internal_source(char **args);
+int internal_jobs();
+
+/*
+ * Función:  imprimir_prompt
+ * -------------------
+ * Imprime el directorio actual + '$'
+ */
+void imprimir_prompt() {
+    char cwd[COMMAND_LINE_SIZE];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s $ ", cwd); // Print del dir. actual + $.
+    } else {
+        perror("getcwd() error");
+    }
+}
+
+/*
+ * Función:  read_line
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+char *read_line(char *line) {
+    imprimir_prompt();
+    fflush(stdout); // Vaciamos el buffer de salida.
+    if (fgets(line, COMMAND_LINE_SIZE, stdin) == NULL) {
+        if (feof(stdin)) { // Comprueba si es EOF aka (Ctrl+D).
+            printf("\nLogout\n");
+            exit(0);
+        } else {
+            perror("fgets() error");
+            exit(1);
+        }
+    }
+    size_t length = strlen(line);
+    if (line[length - 1] == '\n') {
+        line[length - 1] = '\0'; // Sustituye el carácter de nueva línea por un carácter nulo.
+    }
+    return line;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int execute_line(char *line) {
+
+    char *args[MAX_ARGS];
+    parse_args(args, line);
+    return check_internal(args);
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int parse_args(char **args, char *line) {
+    int num_tokens = 0;
+    char *token = strtok(line, " \t");
+    while (token != NULL && num_tokens < MAX_ARGS - 1) {
+        if (token[0] == '#') { //Skipeamos los comments
+            break;
+        }
+        args[num_tokens++] = token;
+        token = strtok(NULL, " \t");
+    }
+    args[num_tokens] = NULL; //Acabamos con un null
+    return num_tokens;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int check_internal(char **args) {
+    if(strcmp(args[0],"exit")==0){
+        exit(0);
+    }else{
+        if (strcmp(args[0], "cd") == 0) {
+        return internal_cd(args);
+    } else if (strcmp(args[0], "export") == 0) {
+        return internal_export(args);
+    } else if (strcmp(args[0], "source") == 0) {
+        return internal_source(args);
+    } else if (strcmp(args[0], "jobs") == 0) {
+        return internal_jobs();
+    }
+    return 0;
+    }
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_cd(char **args) {
+    char cwd[COMMAND_LINE_SIZE];
+    char dir[COMMAND_LINE_SIZE] = {0}; // Inicializo array a zero
+
+    // Caso 1 (Sin argumentos): Cambia al directorio HOME
+    if (args[1] == NULL) {
+        strcpy(dir, getenv("HOME"));
+        if (dir[0] == '\0') {
+            fprintf(stderr, "Error: No se pudo obtener el HOME\n");
+            return -1;
+        }
+    } 
+    // Caso 2 (Un argumento): Cambia al directorio especificado
+    else if (args[2] == NULL) {
+        strcpy(dir, args[1]);
+    } 
+    // Caso 3 (Más de un argumento): Manejar cd avanzado
+    else {
+        //Concatenamos los argumentos para formar el nombre completo del directorio
+        for (int i = 1; args[i] != NULL; i++) {
+            if (i > 1) {
+                strcat(dir, " "); // Añadimos espacios entre argumentos
+            }
+            strcat(dir, args[i]); // Añade el argumento actual
+        }
+        // Manejo comillas y escapes aquí
+        if (dir[0] == '\"' || dir[0] == '\'') {
+            size_t len = strlen(dir);
+            if (dir[len - 1] == dir[0]) {
+                dir[len - 1] = '\0';
+                memmove(dir, dir + 1, len - 1);
+            }
+        }
+    }
+    
+
+    // Cambio de directorio
+    if (chdir(dir) != 0) {
+        perror("chdir() error");
+        return -1;
+    }
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Cambiado al directorio: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        return -1;
+    }
+
+    return 0;
+}
+
+
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_export(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "Error de sintaxis. Uso: export Nombre=Valor\n");
+        return -1;
+    }
+
+    char *name = args[1];
+    char *value = strchr(args[1], '=');
+
+    if (value == NULL || value == name) {
+        fprintf(stderr, "Error de sintaxis. Uso: export Nombre=Valor\n");
+        return -1;
+    }
+
+    // Dividir el nombre y el valor
+    *value = '\0';  // Divide la cadena en el signo igual
+    value++;        // Avanza al valor real
+
+    // Muestra el valor actual
+    char *old_value = getenv(name);
+    printf("[internal_export()→ antiguo valor para %s: %s]\n", name, old_value ? old_value : "(null)");
+
+    // Asigna el nuevo valor
+    if (setenv(name, value, 1) != 0) {
+        perror("setenv() error");
+        return -1;
+    }
+
+    // Muestra el nuevo valor
+    char *new_value = getenv(name);
+    printf("[internal_export()→ nuevo valor para %s: %s]\n", name, new_value ? new_value : "(null)");
+
+    return 0;
+}
+
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_source(char **args) {
+    printf("Ejecutar script '%s'\n", args[1]);
+    return 1;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_jobs() {
+    printf("Listar todos los trabajos en segundo plano.\n");
+    return 1;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_fg(char **args) {
+    printf("Moviendo el trabajo '%s' al primer plano...\n", args[1]);
+    
+    return 1;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int internal_bg(char **args) {
+    printf("Continuando con el trabajo '%s' en segundo plano...\n", args[1]);
+    
+    return 1;
+}
+
+/*
+ * Función:  
+ * -------------------
+ * 
+ *
+ * dest:
+ * src:
+ *
+ * retorna:
+ */
+int main() {
+    char line[COMMAND_LINE_SIZE];
+    while (1) {
+        if (read_line(line)) {
+            execute_line(line);
+        }
+    }
+    return 0;
+}
